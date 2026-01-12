429e294c8c5c6b237411be506df8efaabaeec264
Kushagar <kathuriakush03@gmail.com.com>
2026-01-12 12:37:20 +0530

    Day 6

---
+++ Day6/FrameWorkConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreetingLibrary1;
+namespace FrameWorkConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("hello");
+
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+
+            string message = GreetHelper.GetGreeting(name);
+
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+        }
+    }
+}
