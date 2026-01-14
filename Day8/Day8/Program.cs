65f3ef78ba29e2429ef121a02de423a9e86f60bd
Kushagar <kathuriakush03@gmail.com.com>
2026-01-14 14:31:16 +0530

    day8

---
+++ Day8/Day8/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Day8
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] parts = input.Split('|');
+            string userName = parts[0];
+            string message = parts[1];
+
+            string cleanMessage = message.Trim().ToLower();
+
+            bool containsSuccessful = cleanMessage.Contains("successful");
+
+            string standardMessage = "login successful";
+
+            string status;
+
+            if (!containsSuccessful)
+            {
+                status = "LOGIN FAILED";
+            }
+            else if (cleanMessage.Equals(standardMessage))
+            {
+                status = "LOGIN SUCCESS";
+            }
+            else
+            {
+                status = "LOGIN SUCCESS (CUSTOM MESSAGE)";
+            }
+            Console.WriteLine($"User     : {userName}");
+            Console.WriteLine($"Message  : {cleanMessage}");
+            Console.WriteLine($"Status   : {status}");
+        }
+    }
+}
