61987f97ccd484b40654d3b47455e18c0df6d49f
Kushagar <kathuriakush03@gmail.com.com>
2026-01-16 14:36:35 +0530

    day8 - 02

---
+++ Day8-02/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Day8_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string data = Console.ReadLine();
+
+            string[] items = data.Split('#');
+            string id = items[0];
+            string name = items[1];
+            string text = items[2];
+
+            string result = text.Trim();
+
+            while (result.Contains("  "))
+            {
+                result = result.Replace("  ", " ");
+            }
+
+            result = result.ToLower();
+
+            bool d = result.Contains("deposit");
+            bool w = result.Contains("withdrawal");
+            bool t = result.Contains("transfer");
+
+            bool flag = d || w || t;
+
+            string standard = "cash deposit successful";
+            string output;
+
+            if (!flag)
+            {
+                output = "NON-FINANCIAL TRANSACTION";
+            }
+            else if (result.Equals(standard))
+            {
+                output = "STANDARD TRANSACTION";
+            }
+            else
+            {
+                output = "CUSTOM TRANSACTION";
+            }
+
+            Console.WriteLine($"Transaction ID : {id}");
+            Console.WriteLine($"Account Holder : {name}");
+            Console.WriteLine($"Narration      : {result}");
+            Console.WriteLine($"Category       : {output}");
+        }
+    }
+}
