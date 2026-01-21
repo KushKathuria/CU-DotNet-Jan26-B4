a60902350b73f2a74b6674d6bec62a09044e427e
Kushagar <kathuriakush03@gmail.com.com>
2026-01-21 14:13:42 +0530

    Employee Class

---
+++ Day14/OopsLearning/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace OopsLearning
+{
+
+    class Employee
+    {
+        private int id;
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+        public int getID()
+        {
+            return id;
+        }
+        public string Name { get; set; }
+
+        private string department;
+        public string Department
+        {
+            set
+            {
+                if (value == "Accounts" || value == "Sales" || value == "IT")
+                {
+                    department = value;
+                }
+                else
+                {
+                    department = "Invalid";
+                }
+            }
+            get { return department; }
+
+        }
+        private int salary;
+
+        public int Salary
+        {
+            get { return salary; }
+            set {
+                if (value < 50000 || value > 90000) { salary = 50000; }
+                else
+                {
+                    salary = value;
+
+                }
+            }
+        }
+        public void Display()
+        {
+            Console.WriteLine($"ID :{id}");
+            Console.WriteLine($"Name :{Name}");
+            Console.WriteLine($" Department :{Department}");
+            Console.WriteLine($"Salary :{salary}");
+        }
+
+    }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Employee emp = new Employee();
+            emp.setId(101);
+            emp.Name = "Kush";
+            emp.Department = "HR";
+            emp.Salary = 50;
+            emp.Display();
+
+            }
+        }
+    }
