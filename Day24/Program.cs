558a094434be916f246ef14a92ec50adc89336b9
Kushagar <kathuriakush03@gmail.com.com>
2026-02-03 14:34:51 +0530

    Day24 1 and 2

---
+++ Day24/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+using ClassLibrary1;
+namespace Clasworkk
+{
+    
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+Hashtable table= new Hashtable();
+            table.Add(101, "Alice");
+            table.Add(102, "Bob");
+            table.Add(103, "Charlie");
+            table.Add(104, "Diana");
+
+            if (!table.ContainsKey(105))
+            {
+                table[105] = "Edward";
+            }
+            else
+            {
+                Console.WriteLine($"{105} : {table[105]}");
+            }
+
+            string SampleName = (string)table[102];
+
+            Console.WriteLine($"Name[102]:  {SampleName}");
+            foreach(DictionaryEntry i in table)
+            {
+                Console.WriteLine($"Id: {i.Key}  Name: {i.Value}");
+            }
+
+            table.Remove(103);
+            int tableCount=table.Count;
+            Console.WriteLine($"Table count :{tableCount}");
+                }
+
+    }
+}
+
+
+
+
+
+
---
+++ Day24/newClassWork.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clasworkk
+{
+    internal class newClassWork
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double, string> leaderboard= new SortedDictionary<double, string>();
+            leaderboard.Add(55.42, "SwiftRacer");
+            leaderboard.Add(52.10, "SpeedDemon");
+            leaderboard.Add(58.91, "SteadyEddie");
+            leaderboard.Add(51.05, "TurboTom");
+
+            foreach (KeyValuePair<double,string> i in leaderboard) {
+                Console.WriteLine($"LapTime :{i.Key:f2}  Name: {i.Value}");                 
+            }
+            double winner= leaderboard.Keys.First();
+            Console.WriteLine($"Winner: {winner}\n" +
+                $"Name: {leaderboard[winner]}");
+
+            leaderboard.Remove(58.91);
+            leaderboard.Add(54.00, "SteadyEddie");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Updated Record");
+            foreach (KeyValuePair<double, string> i in leaderboard)
+            {
+                Console.WriteLine($"LapTime :{i.Key:f2}  Name: {i.Value}");
+            }
+
+        }
+    }
+}
