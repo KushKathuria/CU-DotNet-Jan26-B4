89c0d124b9e441d83b140c7c17acc1ed8c88e7e1
Kushagar <kathuriakush03@gmail.com.com>
2026-02-03 15:06:00 +0530

    Updated day 24

--- Day24/newClassWork.cs
+++ Day24/newClassWork.cs
@@ -11,20 +11,24 @@ namespace Clasworkk
     {
         static void Main(string[] args)
         {
-            SortedDictionary<double, string> leaderboard= new SortedDictionary<double, string>();
+            SortedDictionary<double, string> leaderboard = new SortedDictionary<double, string>();
             leaderboard.Add(55.42, "SwiftRacer");
             leaderboard.Add(52.10, "SpeedDemon");
             leaderboard.Add(58.91, "SteadyEddie");
             leaderboard.Add(51.05, "TurboTom");
 
-            foreach (KeyValuePair<double,string> i in leaderboard) {
-                Console.WriteLine($"LapTime :{i.Key:f2}  Name: {i.Value}");                 
+            foreach (KeyValuePair<double, string> i in leaderboard)
+            {
+                Console.WriteLine($"LapTime :{i.Key:f2}  Name: {i.Value}");
             }
-            double winner= leaderboard.Keys.First();
+            double winner = leaderboard.Keys.First();
             Console.WriteLine($"Winner: {winner}\n" +
                 $"Name: {leaderboard[winner]}");
+            bool id = leaderboard.ContainsValue("SteadyEddie");
+            var sa = leaderboard.FirstOrDefault(x => x.Value.Equals("SteadyEdd")).Key;
 
-            leaderboard.Remove(58.91);
+            Console.WriteLine(sa);
+            leaderboard.Remove(sa);
             leaderboard.Add(54.00, "SteadyEddie");
             Console.WriteLine("------------------------------------");
             Console.WriteLine("Updated Record");
