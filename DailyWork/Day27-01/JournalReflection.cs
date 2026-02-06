1e936a8d16f5a3e154368d7edc132bc575787879
Kushagar <kathuriakush03@gmail.com.com>
2026-02-06 16:08:35 +0530

    Day 27 added

---
+++ DailyWork/Day27-01/JournalReflection.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Channels;
+
+namespace Classworkk
+{
+
+    internal class JournalReflection
+    {
+        static void Main(string[] args)
+        {
+            string path = @"..\..\..\Journal.txt";
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+
+                Console.WriteLine("Enter Data");
+                string data = Console.ReadLine();
+                sw.WriteLine(data);
+
+            }
+
+            Console.WriteLine("--------------Reflection--------------\n");
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string Line;
+                while ((Line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(Line);
+                }
+            }
+        }
+    }
+}
+//C:\Users\KUSH\source\repos\SolutionFolder\SolutionFolder\Clasworkk\file1.text
---
+++ Project/MiniProject1/WordHunter/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordHunter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Lives = 6;
+
+            List<string> input = new List<string>() { "CODE", "ASTRONOMY", "SCALE","PICKACHU","LIBRARY","KEYBOARD","SOFTWARE","FRAMEWORK","ALGORITHM","POINTER" };
+            Random random = new Random();
+            string HiddenWord = input[random.Next(input.Count)];
+            char[] Word = HiddenWord.ToCharArray();
+
+            HashSet<char> Guessed = new HashSet<char>();
+
+            char[] Display = new char[HiddenWord.Length];
+            for (int i = 0; i < Display.Length; i++)
+                Display[i] = '_';
+
+            Console.WriteLine("Welcome To My Game");
+
+            while (Lives > 0 && new string(Display) != new string(Word))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Word: " + string.Join(" ", Display));
+                Console.WriteLine($"Lives Remaining: {Lives}");
+                Console.WriteLine("Guessed: " + string.Join(", ", Guessed));
+
+                Console.Write("Enter a letter: ");
+                string userInput = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(userInput))
+                {
+                    Console.WriteLine("Input cannot be empty or whitespace.");
+                    continue;
+                }
+
+                if (userInput.Length != 1)
+                {
+                    Console.WriteLine("Please enter exactly one letter.");
+                    continue;
+                }
+
+                char letter = char.ToUpper(userInput[0]);
+
+                if (!char.IsLetter(letter))
+                {
+                    Console.WriteLine("Not a valid letter. Try again.");
+                    continue;
+                }
+
+                if (Guessed.Contains(letter))
+                {
+                    Console.WriteLine("You already guessed that letter.");
+                    continue;
+                }
+
+                Guessed.Add(letter);
+
+                bool foundLetter = false;
+
+                for (int i = 0; i < Word.Length; i++)
+                {
+                    if (Word[i] == letter)
+                    {
+                        Display[i] = letter;
+                        foundLetter = true;
+                    }
+                }
+
+                if (!foundLetter)
+                {
+                    Lives--;
+                    Console.WriteLine("Wrong letter!");
+                }
+            }
+
+            Console.WriteLine();
+
+            if (new string(Display) == new string(Word))
+            {
+                Console.WriteLine($"You guessed the right word: {HiddenWord}");
+            }
+            else
+            {
+                Console.WriteLine($"You lose!! Better luck next time.\nThe right word was: {HiddenWord}");
+            }
+        }
+    }
+}
