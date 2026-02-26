ddf23803dc3134ca301b471456d92b06cf27aac2
Kushagar <kathuriakush03@gmail.com.com>
2026-02-27 00:23:29 +0530

    CoverUp

---
+++ DailyWork/Week1 and 2/Day13-01/GymMemberShip/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace GymMemberShip
+{
+    internal class Program
+    {
+        public static double CalculateMembershipAmount(
+    bool treadmill,
+    bool weightLifting,
+    bool zumba)
+        {
+            double total = 1000; 
+
+            bool anyServiceSelected = false;
+
+            if (treadmill)
+            {
+                total += 300;
+                anyServiceSelected = true;
+            }
+
+            if (weightLifting)
+            {
+                total += 500;
+                anyServiceSelected = true;
+            }
+
+            if (zumba)
+            {
+                total += 250;
+                anyServiceSelected = true;
+            }
+
+            if (!anyServiceSelected)
+            {
+                total += 200;
+            }
+
+            total += total * 0.05;
+
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            double amount = CalculateMembershipAmount(true, false, true);
+            Console.WriteLine($"Total Monthly Amount: {amount:c2}");
+        }
+    }
+}
